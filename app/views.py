6fd70274e940df2a310607b3897d9c2b9b874f2c
unknown <martynasvaznonis@gmail.com>
2022-12-08 19:51:48 +0000

    Added songs of the day

--- app/views.py
+++ app/views.py
@@ -3,12 +3,27 @@ from flask import render_template
 from flask_admin.contrib.sqla import ModelView
 from .models import User, Artist, Album, Song
 
+from random import choice, choices, seed
+from datetime import date
+from time import mktime
+
 admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Artist, db.session))
 admin.add_view(ModelView(Album, db.session))
 admin.add_view(ModelView(Song, db.session))
 
 
+def recommendations():
+    seed(mktime(date.today().timetuple()))
+    artists = choices(Artist.query.all(), k=3)
+    albums = [choice(artist.albums) for artist in artists]
+    songs = [choice(album.songs) for album in albums]
+    return [
+        {'artist': artist, 'song': song, 'album': album}
+        for artist, album, song
+        in zip(artists, albums, songs)
+        ]
+
 @app.route('/')
 def home():
-    return render_template('music.html')
+    return render_template('home.html', recommendations=recommendations())
